9a4e620c5e9e17ca09ab174716d0a26f2edb77e7
Esteban Alvarez <aeap19980929@gmail.com>
2020-05-25 15:41:23 -0500

    Corrigiendo ciclos

--- cicle/cicle.go
+++ cicle/cicle.go
@@ -11,6 +11,16 @@ import (
 )
 
 /*
+	format da formato a la duracion y al contador.
+*/
+func format(duration time.Duration, cont int) string {
+	h := duration / time.Hour
+	duration -= h * time.Hour
+	m := duration / time.Minute
+	return fmt.Sprintf("\r[%02d:%02d] | %040d", h, m, cont)
+} 
+
+/*
 	WithLimit realiza el ciclo mostrando un contador por un tiempo limite dado por el usuario
 */
 func WithLimit () {
@@ -21,37 +31,27 @@ func WithLimit () {
 	input_limit, _ := reader_limit.ReadString('\n') 			// Leer hasta el separador de salto de línea
 	selection_limit := strings.TrimRight(input_limit, "\r\n") 	// Remover el salto de línea de la entrada del usuario
 	limit, _ := strconv.Atoi(selection_limit)
-	
 	if limit > 5 {
 		fmt.Println("\rDebe ser un valor igual o menor a 5")
 		return 
 	} 
-	selection_limit = strconv.Itoa(limit) + "m"
+	limit = limit * 60000
+	selection_limit = strconv.Itoa(limit) + "ms"
 	limitms, _ := time.ParseDuration(selection_limit)
 	screen.Clear() 
 	screen.MoveTopLeft()
-	tf := time.Now().Add(limitms)
-
+	t0 := time.Now()
 	for {
-		tn := time.Now()
-		if (tn.After(tf)) {					// Valida fin del ciclo
+		t1 := time.Now()
+		tt := t1.Sub(t0)
+		durStr := format(tt, cont)
+		fmt.Print(durStr) 
+		if (limitms < tt) {				// Valida fin del ciclo
 			break
 		}							
-		fmt.Printf("\r%040d", cont) 
 		cont++ 
 	}
 }
-
-/*
-	format da formato a la duracion y al contador.
-*/
-func format(duration time.Duration, cont int) string {
-	duration = duration.Round(time.Minute)
-	h := duration / time.Hour
-	duration -= h * time.Hour
-	m := duration / time.Minute
-	return fmt.Sprintf("\r[%02d:%02d] | %040d", h, m, cont)
-} 
 
 /*
 	WithoutLimit realiza un ciclo infinito imprimiendo un cronometro y un contador. 
@@ -60,12 +60,8 @@ func format(duration time.Duration, cont int) string {
 func WithoutLimit () {
 	cont := 0
 	screen.MoveTopLeft()
-	t0 := time.Now()
 	for {
-		t1 := time.Now()
-		tt := t1.Sub(t0)
-		durStr := format(tt, cont)
-		fmt.Print(durStr)
+		fmt.Printf("\r%040d", cont)
 		cont++ 
 	}
 }
